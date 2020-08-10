5b4059cc938d61bf7a0717b65ce0fd6de89c8d4a
Demba Siby <dembasiby@gmail.com>
2020-08-10 17:28:21 +0000

    Ajout text traduction supplementaire

--- resources/lang/fr/cruds.php
+++ resources/lang/fr/cruds.php
@@ -105,6 +105,7 @@ return [
             'updated_at_helper' => '',
             'deleted_at'        => 'Deleted at',
             'deleted_at_helper' => '',
+            'team_helper'       => '',
         ],
     ],
     'strategie'             => [
