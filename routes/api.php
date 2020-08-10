749bd3fdce73148e4d99f27f673ad729c02f1e57
Demba Siby <dembasiby@gmail.com>
2020-08-10 17:27:27 +0000

    Desactivation routes API

--- routes/api.php
+++ routes/api.php
@@ -1,48 +1,48 @@
 <?php
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function () {
-    // Permissions
-    Route::apiResource('permissions', 'PermissionsApiController');
+/* Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function () { */
+/*     // Permissions */
+/*     Route::apiResource('permissions', 'PermissionsApiController'); */
 
-    // Roles
-    Route::apiResource('roles', 'RolesApiController');
+/*     // Roles */
+/*     Route::apiResource('roles', 'RolesApiController'); */
 
-    // Users
-    Route::apiResource('users', 'UsersApiController');
+/*     // Users */
+/*     Route::apiResource('users', 'UsersApiController'); */
 
-    // Teams
-    Route::apiResource('teams', 'TeamApiController');
+/*     // Teams */
+/*     Route::apiResource('teams', 'TeamApiController'); */
 
-    // Organisations
-    Route::apiResource('organisations', 'OrganisationsApiController');
+/*     // Organisations */
+/*     Route::apiResource('organisations', 'OrganisationsApiController'); */
 
-    // Effet Intermediaires
-    Route::apiResource('effet-intermediaires', 'EffetIntermediairesApiController');
+/*     // Effet Intermediaires */
+/*     Route::apiResource('effet-intermediaires', 'EffetIntermediairesApiController'); */
 
-    // Effet Immediats
-    Route::apiResource('effet-immediats', 'EffetImmediatsApiController');
+/*     // Effet Immediats */
+/*     Route::apiResource('effet-immediats', 'EffetImmediatsApiController'); */
 
-    // Extrants
-    Route::apiResource('extrants', 'ExtrantsApiController');
+/*     // Extrants */
+/*     Route::apiResource('extrants', 'ExtrantsApiController'); */
 
-    // Resultat Intermediaires
-    Route::apiResource('resultat-intermediaires', 'ResultatIntermediairesApiController');
+/*     // Resultat Intermediaires */
+/*     Route::apiResource('resultat-intermediaires', 'ResultatIntermediairesApiController'); */
 
-    // Probleme Centrals
-    Route::apiResource('probleme-centrals', 'ProblemeCentralsApiController');
+/*     // Probleme Centrals */
+/*     Route::apiResource('probleme-centrals', 'ProblemeCentralsApiController'); */
 
-    // Indicateurs
-    Route::apiResource('indicateurs', 'IndicateursApiController');
+/*     // Indicateurs */
+/*     Route::apiResource('indicateurs', 'IndicateursApiController'); */
 
-    // Type Questions
-    Route::apiResource('type-questions', 'TypeQuestionsApiController');
+/*     // Type Questions */
+/*     Route::apiResource('type-questions', 'TypeQuestionsApiController'); */
 
-    // Questions
-    Route::apiResource('questions', 'QuestionsApiController');
+/*     // Questions */
+/*     Route::apiResource('questions', 'QuestionsApiController'); */
 
-    // Questionnaires
-    Route::apiResource('questionnaires', 'QuestionnairesApiController');
+/*     // Questionnaires */
+/*     Route::apiResource('questionnaires', 'QuestionnairesApiController'); */
 
-    // Reponses
-    Route::apiResource('reponses', 'ReponsesApiController');
-});
+/*     // Reponses */
+/*     Route::apiResource('reponses', 'ReponsesApiController'); */
+/* }); */
