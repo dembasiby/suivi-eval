e77b01813c3f8676639755136477c830d2a20b4f
Demba Siby <dembasiby@gmail.com>
2020-08-31 15:49:06 +0000

    Added aggregate tables to dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,44 +2,52 @@
 
 namespace App\Http\Controllers\Admin;
 
-use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use App\Models\Indicateur;
+use App\Models\Reponse;
+use App\Models\Question;
 
 class HomeController
 {
     public function index()
     {
-        $settings1 = [
-            'chart_title'        => 'My Report 1',
-            'chart_type'         => 'bar',
-            'report_type'        => 'group_by_relationship',
-            'model'              => 'App\\Models\\Reponse',
-            'group_by_field'     => 'description',
-            'aggregate_function' => 'count',
-            'filter_field'       => 'created_at',
-            'filter_period'      => 'year',
-            'column_class'       => 'col-md-6',
-            'entries_number'     => '5',
-            'relationship_name'  => 'question',
-        ];
+        // $settings1 = [
+            // 'chart_title'        => 'My Report 1',
+            // 'chart_type'         => 'bar',
+            // 'report_type'        => 'group_by_relationship',
+            // 'model'              => 'App\\Models\\Reponse',
+            // 'group_by_field'     => 'description',
+            // 'aggregate_function' => 'count',
+            // 'filter_field'       => 'created_at',
+            // 'filter_period'      => 'year',
+            // 'column_class'       => 'col-md-6',
+            // 'entries_number'     => '5',
+            // 'relationship_name'  => 'question',
+        // ];
 
-        $chart1 = new LaravelChart($settings1);
+        // $chart1 = new LaravelChart($settings1);
 
-        $settings2 = [
-            'chart_title'        => 'My Report 2',
-            'chart_type'         => 'line',
-            'report_type'        => 'group_by_relationship',
-            'model'              => 'App\\Models\\Question',
-            'group_by_field'     => 'description',
-            'aggregate_function' => 'count',
-            'filter_field'       => 'created_at',
-            'filter_period'      => 'year',
-            'column_class'       => 'col-md-6',
-            'entries_number'     => '5',
-            'relationship_name'  => 'indicateur',
-        ];
+        // $settings2 = [
+            // 'chart_title'        => 'My Report 2',
+            // 'chart_type'         => 'line',
+            // 'report_type'        => 'group_by_relationship',
+            // 'model'              => 'App\\Models\\Question',
+            // 'group_by_field'     => 'description',
+            // 'aggregate_function' => 'count',
+            // 'filter_field'       => 'created_at',
+            // 'filter_period'      => 'year',
+            // 'column_class'       => 'col-md-6',
+            // 'entries_number'     => '5',
+            // 'relationship_name'  => 'indicateur',
+        // ];
 
-        $chart2 = new LaravelChart($settings2);
+        // $chart2 = new LaravelChart($settings2);
+        $indicateurs = Indicateur::paginate(25);
 
-        return view('home', compact('chart1', 'chart2'));
+        $reponses = Reponse::all();
+        // $reponses = $reponses->groupBy('description');
+        // dd($reponses);
+
+
+        return view('home', compact('indicateurs', 'reponses'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Dashboard
+                    Tableau de bord
                 </div>
 
                 <div class="card-body">
@@ -14,24 +14,48 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <div class="row">
-                        <div class="{{ $chart1->options['column_class'] }}">
-                            <h3>{!! $chart1->options['chart_title'] !!}</h3>
-                            {!! $chart1->renderHtml() !!}
-                        </div>
-                        <div class="{{ $chart2->options['column_class'] }}">
-                            <h3>{!! $chart2->options['chart_title'] !!}</h3>
-                            {!! $chart2->renderHtml() !!}
-                        </div>
-                    </div>
+                </div>
+                <div class="container">
+                   <ul>
+                        @foreach($indicateurs as $indicateur)
+                            <li style="list-style:none">
+                                <h5>{{$indicateur->code_indicateur}} - {{ $indicateur->description }}</h5>
+                                @foreach($indicateur->indicateurQuestions as $question)
+                                    @if( $question->options != null)
+                                        <table class="table w-80">
+                                            <thead class="thead-dark">
+                                                <tr>
+                                                @foreach($question->options as $key)
+                                                    <th>{{ $key }}</th>
+                                                @endforeach
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                @foreach($question->options as $key)
+                                                    @if(Str::startsWith($key, 'Nombre'))
+                                                        <td>{{ $question->reponses->sum('description.'. $key)}}</td>
+                                                    @else
+                                                        <td>
+                                                            {{ $question->reponses->first() }}
+                                                        </td>
+                                                    @endif
+                                                @endforeach
+                                                </tr>
+                                            </tbody>
+                                         </table>
+                                         @else
+                                            @foreach($question->reponses as $reponse)
+                                                <p>{{ $reponse->description }}</p>
+                                            @endforeach
+                                         @endif 
+                                @endforeach
+                            </li>
+                        @endforeach
+                   </ul> 
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
-@section('scripts')
-@parent
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>{!! $chart1->renderJs() !!}{!! $chart2->renderJs() !!}
 @endsection
